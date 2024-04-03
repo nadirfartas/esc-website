6090ef96965bbf003422cf4fbbae776ad2378184
ayakhiouat <ayakhiouat@gmail.com>
2024-04-03 03:35:19 +0100

    Fixing structure and more bugs

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import '../../App.css';
-import HeroSection from '../HeroSection';
 
 
 function Home() {
   return (
     <>
-     <HeroSection />
+     
     </>
   );
 };
