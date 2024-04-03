c0cad94c082e5362ca52ae214932899bbc5d3f4a
ayakhiouat <ayakhiouat@gmail.com>
2024-04-03 18:28:10 +0100

    er & hr pages added

---
+++ src/components/departments/exernal.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import Particle from "../Particle";
+import laptopImg from "../../images/usthb-logo.png";
+
+function external() {
+  return (
+    <Container fluid className="about-section">
+      <Particle />
+      <Container>
+        <Row style={{ justifyContent: "center", padding: "10px" }}>
+          <Col
+            md={7}
+            style={{
+              justifyContent: "center",
+              paddingTop: "30px",
+              paddingBottom: "50px",
+            }}
+          >
+            <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
+              External Relations
+            </h1>
+          </Col>
+          <Col
+            md={5}
+            style={{ paddingTop: "120px", paddingBottom: "50px" }}
+            className="about-img"
+          >
+            <img src={laptopImg} alt="about" className="img-fluid" />
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default external;
