91902226b12498a71a956ae6f2d4e4083c8066ae
ayakhiouat <ayakhiouat@gmail.com>
2024-04-03 03:04:14 +0100

    more bug fixes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -47,7 +47,7 @@ function Navbar() {
                 className='nav-links'
                 onClick={closeMobileMenu}
               >
-                Services
+                Activities
               </Link>
             </li>
             <li className='nav-item'>
@@ -56,7 +56,7 @@ function Navbar() {
                 className='nav-links'
                 onClick={closeMobileMenu}
               >
-                Products
+                Events
               </Link>
             </li>
 
@@ -66,7 +66,7 @@ function Navbar() {
                 className='nav-links-mobile'
                 onClick={closeMobileMenu}
               >
-                Sign Up
+                Join us
               </Link>
             </li>
           </ul>
